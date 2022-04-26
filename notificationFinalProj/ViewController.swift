84b2fb2580bc5402dc86262bcb90ad78eb8297b2
AndrewL123456 <andrewlujan763@gmail.com>
2022-04-26 12:56:09 -0600

    Initial Commit

---
+++ notificationFinalProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  notificationFinalProj
+//
+//  Created by Lujan, Andrew - Student on 4/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
